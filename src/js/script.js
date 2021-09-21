e2f00e6f5cc082ec66f04616d0e475cc0c172bcb
Szymon Wierucki <szymon.wierucki@gmail.com>
2021-09-21 23:34:36 +0200

    Add code for main page and fix js code

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 
 // eslint-disable-next-line no-undef
-var chart = new Chart(ctx, {
+new Chart(ctx, {
   // 1
   type: 'bar',
   data: {
@@ -35,3 +35,53 @@ var chart = new Chart(ctx, {
     }]
   },
 });
+
+// Menu
+
+function toggleMenu() {
+  const navButton = document.querySelector('.menu');
+  const sidenav = document.querySelector('.sidebar-menu');
+
+  navButton.addEventListener('click', function(e){
+    e.preventDefault();
+
+    sidenav.classList.toggle('nav-active');
+  });
+}
+toggleMenu();
+
+// Funkcja przełączania stron (obecnie nieużywana)
+
+// const navLinks = document.querySelectorAll('.navigation-list .nav-elem');
+// const pages = document.querySelectorAll('.page');
+
+// const titleClickHandler = function (event) {
+//   event.preventDefault();
+//   const clickedPage = this;
+//   console.log('link was clicked');
+//   console.log(event);
+//   console.log(navLinks);
+//   console.log(pages);
+  
+
+//   const activeLinks = document.querySelectorAll('.nav-elem a.active');
+
+//   for (let activeLink of activeLinks) {
+//     activeLink.classList.remove('active');
+//   }
+//   clickedPage.classList.add('active');
+
+//   const activePages = document.querySelectorAll('.active');
+//   for (let activePage of activePages) {
+//     activePage.classList.remove('active');
+//   }
+
+//   const pageSelector = clickedPage.getAttribute('href');
+//   const targetPage = document.querySelector(pageSelector);
+//   targetPage.classList.add('active');
+// };
+// console.log(titleClickHandler);
+// const links = document.querySelectorAll('.navigation-list a');
+// for (let link of links) {
+//   link.addEventListener('click', titleClickHandler);
+// }
